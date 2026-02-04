892eeaf57f68faabd9942c8eed870594812961c4
Mike Oliphant <oliphant@nostatic.org>
2026-02-04 09:12:21 -0800

    Add SkiaLayout DefaultTextureNamespace

--- UILayout.Skia/SkiaLayout.cs
+++ UILayout.Skia/SkiaLayout.cs
@@ -7,12 +7,13 @@ namespace UILayout
     public class SkiaLayout : Layout
     {
         public static new SkiaLayout Current { get { return Layout.Current as SkiaLayout; } }
+        public static string DefaultTextureNamespace { get; set; } = "UILayout";
 
         public SkiaLayout()
         {
             GraphicsContext = new GraphicsContext2D();
 
-            var loader = new AssemblyResourceContentLoader(typeof(UILayout.DefaultTextures.TextureLoader).Assembly, "UILayout");
+            var loader = new AssemblyResourceContentLoader(typeof(UILayout.DefaultTextures.TextureLoader).Assembly, DefaultTextureNamespace);
 
             UILayout.DefaultTextures.TextureLoader.LoadDefaultTextures(loader);
 
